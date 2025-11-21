e7d4e76f947a5d37775424a45b3fb158002176a0
Tanmay Patel <tanmaypatel5897@gmail.com>
2025-11-21 10:29:20 +0530

    Us

--- backend/Config/db.js
+++ backend/Config/db.js
@@ -1,12 +1,20 @@
-  import { Sequelize } from "sequelize"
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
-  const db = new Sequelize("eauction", "root", "", {
-    host: "localhost",
-    dialect: "mysql",
-  }) 
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+  }
+);
 
-  db.authenticate()
-  .then(()=> console.log("MySQL Connected"))
-  .catch((err) => console.log("Error: "+err))
+db.authenticate()
+  .then(() => console.log("MySQL Connected"))
+  .catch((err) => console.log("Error: " + err));
 
-  export default db
+export default db;
